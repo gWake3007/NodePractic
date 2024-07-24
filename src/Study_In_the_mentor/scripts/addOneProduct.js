dac4f11e8d87daf70e567b1c168d7a7e6a9d9384
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-07-24 17:49:18 +0200

    1

--- src/Study_In_the_mentor/scripts/addOneProduct.js
+++ src/Study_In_the_mentor/scripts/addOneProduct.js
@@ -1,14 +1,28 @@
 import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/products.js';
+import { createFakeProduct } from '../utils/createFakeProduct.js';
 
 export const addOneProduct = async () => {
-  let currentProducts;
   try {
-    const data = await fs.readFile(PATH_DB, 'utf-8');
-    console.log('data:', data);
-    currentProducts = data ? JSON.parse(data) : [];
-    console.log('current:', currentProducts);
+    let currentProducts;
+    try {
+      const data = await fs.readFile(PATH_DB, 'utf-8');
+      currentProducts = data ? JSON.parse(data) : [];
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw error;
+      }
+    }
+    const oneProduct = createFakeProduct();
+    currentProducts.push(oneProduct);
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(currentProducts, null, 2),
+      'utf-8',
+    );
   } catch (error) {
     console.error(error);
   }
 };
+
+addOneProduct();
