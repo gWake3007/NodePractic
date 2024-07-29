a92f8d5291604582a2940a6a6abef5937a8fbd05
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-07-29 08:35:32 +0200

    1

--- src/Lesson_4/02-Mongodb/index.js
+++ src/Lesson_4/02-Mongodb/index.js
@@ -1,7 +1,20 @@
+import 'dotenv/config';
 import express from 'express';
+
+import { initDBConnection } from './db.js';
 
 const app = express();
 
-app.listen(8080, () => {
-  console.log('Server started on PORT 8080');
-});
+async function bootstrap() {
+  try {
+    await initDBConnection();
+
+    app.listen(8080, () => {
+      console.log('Server started on PORT 8080');
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+bootstrap();
