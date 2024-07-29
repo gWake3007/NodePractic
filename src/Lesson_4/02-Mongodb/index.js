044fc547412e187c7b5d8c12d35e17989e6d553e
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-07-29 10:38:38 +0200

    1

--- src/Lesson_4/02-Mongodb/index.js
+++ src/Lesson_4/02-Mongodb/index.js
@@ -2,8 +2,37 @@ import 'dotenv/config';
 import express from 'express';
 
 import { initDBConnection } from './db.js';
+import { Student } from './models/students.js';
 
 const app = express();
+
+app.get('/students', async (req, res) => {
+  try {
+    const students = await Student.find();
+
+    res.send({ status: 200, data: students });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Internal Server Error!' });
+  }
+});
+
+app.get('/students/:id', async (req, res) => {
+  try {
+    //?За допомогою деструкторизація з request дістаємо динамічний параметр id.
+    const { id } = req.params;
+    //?findById() - Повертає або об'єкт який він знайшов або null.
+    const student = await Student.findById(id);
+    if (student === null) {
+      return res
+        .status(404)
+        .send({ status: 404, message: 'Student not found!' });
+    }
+    res.send({ status: 200, data: student });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 async function bootstrap() {
   try {
