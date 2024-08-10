4f5c0bbed6914429e08414054d20ddf132225caa
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-08-10 20:37:24 +0200

    1

--- src/Study_In_the_mentor_Lesson_2/src/db/models/product.js
+++ src/Study_In_the_mentor_Lesson_2/src/db/models/product.js
@@ -20,7 +20,7 @@ const productSchema = new mongoose.Schema(
       type: String,
     },
   },
-  { timestamps: true },
+  { timestamps: true, versionKey: false },
 );
 
 const Product = mongoose.model('Product', productSchema);
--- src/Study_In_the_mentor_Lesson_2/src/services/products.js
+++ src/Study_In_the_mentor_Lesson_2/src/services/products.js
@@ -4,6 +4,7 @@ export async function getProducts() {
   return await Product.find();
 }
 
+//?findOne - схожий на метод findById але він повертає об'єкт параметрів.
 export async function getProduct(id) {
-  return await Product.findById(id);
+  return await Product.findOne({ _id: id });
 }
